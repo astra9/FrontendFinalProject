f8b3516bbfc2d3e48b4332d5274850d3ab0082a6
astra9 <thedigitalwhiterabbit@gmail.com>
2022-12-07 06:33:05 -0800

    adding base structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component'; 
+import { RegistrationComponent } from './components/registration/registration.component';
+import { ChatComponent } from './components/chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", pathMatch:"full", redirectTo: "/signin"},
+  {path:"signin", component: SigninComponent},
+  {path:"register", component: RegistrationComponent},
+  {path:"char", component: ChatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    NavigationComponent,
+    SigninComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
